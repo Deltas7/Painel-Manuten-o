57974c28a75c3fec34ceb318ff1a32e89a3632a7
Gabriel Delturqui <gabriel.delturqui@gmail.com>
2026-09-17 16:25:36 -0300

    Update Supabase configuration with new URL

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Configuração de acesso ao Supabase
-const supabaseUrl = 'sb_publishable_2WluWKY6T2l0Zn5JUazT9Q_XwtLwlSM'
-const supabaseKey = ''
+const supabaseUrl = 'https://ueouphbjiylhvlqqknco.supabase.co'
+const supabaseKey = 'sb_publishable_2WluWKY6T2l0Zn5JUazT9Q_XwtLwlSM'
 
 // Inicializa o cliente do Supabase globalmente
 const supabase = supabase.createClient(supabaseUrl, supabaseKey)
