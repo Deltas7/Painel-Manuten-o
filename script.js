6e96b8ce82c5c288d45364c2b3f449134bb298a2
Gabriel Delturqui <gabriel.delturqui@gmail.com>
2026-09-17 16:50:20 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Configuração de acesso ao Supabase
-const supabaseUrl = 'https://ueouphbjiylhvlqqknco.supabase.co'
-const supabaseKey = 'sb_publishable_2WluWKY6T2l0Zn5JUazT9Q_XwtLwlSM'
-
-// Inicializa o cliente do Supabase globalmente
-const supabase = supabase.createClient(supabaseUrl, supabaseKey)
-
