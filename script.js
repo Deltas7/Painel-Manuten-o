f199c65ccd3b37b031a63c9ded959c56d17f1012
Gabriel Delturqui <gabriel.delturqui@gmail.com>
2026-09-17 16:22:51 -0300

    Remove Supabase secret key from script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Configuração de acesso ao Supabase
 const supabaseUrl = 'sb_publishable_2WluWKY6T2l0Zn5JUazT9Q_XwtLwlSM'
-const supabaseKey = 'sb_secret_4PJ0tZ6E_kGHl_wU9wYgrg_a2ZpQ8-X'
+const supabaseKey = ''
 
 // Inicializa o cliente do Supabase globalmente
 const supabase = supabase.createClient(supabaseUrl, supabaseKey)
